9fbb5e6b0167437c563ffde405106b5559943326
Arafat <arafat97alim@gmail.com>
2021-07-26 01:39:14 +0530

    creating a card component with styling using tachyons classname.

---
+++ src/Cards.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Cards = () => {
+  return (
+    <div className="bg-light-green dib pa3 ma2 grow bw2 shadow-5">
+      <img src="https://robohash.org/test?200x200" alt="robots" />
+      <div>
+        <h2>Arafat Alim</h2>
+        <p>arafat97alim@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
